a3426cdcd57c10a9ab94403e3089d4201e8d4916
thomasbalcazas <balcazasthomas@gmail.com>
2025-06-10 15:01:24 -0300

    Add files via upload

---
+++ Codigo/DBA Njoy.sql
@@ -0,0 +1,28 @@
+create database Njoy
+
+CREATE TABLE usuario (
+Id_usuario  NUMERIC PRIMARY KEY NOT NULL,
+Nombre_Apellido VARCHAR NOT NULL,
+correo_electronico  VARCHAR NOT NULL,
+contrasena  VARCHAR NOT NULL,
+fecha_registro DATE NOT NULL,
+rol VARCHAR NOT NULL);
+
+CREATE TABLE Juego (
+id_juego NUMERIC PRIMARY KEY NOT NULL,
+titulo VARCHAR NOT NULL,
+descripcion  VARCHAR NOT NULL,
+precio  NUMERIC NOT NULL,
+fecha_lanzamiento DATE NOT NULL,
+genero VARCHAR NOT NULL,
+plataforma  VARCHAR NOT NULL);
+
+CREATE TABLE resena (
+id_resena NUMERIC PRIMARY KEY NOT NULL,
+id_usuario NUMERIC NOT NULL,
+Id_usuario NUMERIC NOT NULL,
+calificacion  NUMERIC NOT NULL,
+comentario  VARCHAR NOT NULL,
+fecha_resena DATETIME NOT NULL,
+FOREIGN KEY(id_usuario) REFERENCES usuario(Id_usuario ),
+FOREIGN KEY(Id_usuario) REFERENCES Juego(id_juego));
